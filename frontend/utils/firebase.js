8d604d01fba8792575798365b2ca8ee0d4c763bd
Aman Kumar <amankumar9142424868@gmail.com>
2026-03-25 16:45:49 +0530

    onlinelms

---
+++ frontend/utils/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: "loginonlinems.firebaseapp.com",
+  projectId: "loginonlinems",
+  storageBucket: "loginonlinems.firebasestorage.app",
+  messagingSenderId: "194248124287",
+  appId: "1:194248124287:web:dca1ad0d148d7e747c5c4a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app)
+const provider = new GoogleAuthProvider()
+
+export {auth, provider}
